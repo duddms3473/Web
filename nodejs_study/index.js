20d71ff9518714883365c8e6ae84ed84ab87095b
Lee Young Eun <77182479+duddms3473@users.noreply.github.com>
2023-10-11 18:10:47 +0900

    Update index.js

--- nodejs_study/index.js
+++ nodejs_study/index.js
@@ -1,25 +1,31 @@
-
+var cors = require('cors')
 const express = require('express')
 const app = express()
 const port=3000
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('<a href="https://github.com/duddms3473">깃허브로 이동!</a>')
 })
 
-app.get('/user/:id', function (req, res) {
-  // const q=req.params
-  // console.log(q.id)
-  
-  const q=req.query
-  console.log(q)
-  console.log(q.name)
+app.get('/sound/:name', function (req, res) {
+  const { name }=req.params
+  console.log(name)
 
-  res.json({'userid':q.age})
-})
+  if(name=="dog"){
+    res.json({'sound':'멍멍'})
+  }
+  else if(name=="cat"){
+    res.json({'sound':'야옹'})
+  }
+  else if(name=="cat"){
+    res.json({'sound':'꿀꿀'})
+  }
+  else {
+    res.json({'sound':'알 수 없음'})
+  }
 
-app.get('/cat', function (req, res) {
-  res.json({'sound':'멍멍'})
 })
 
 app.listen(port,()=>{
